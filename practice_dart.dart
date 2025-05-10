9d5b51dab180f5431bea78d1f05364eeb345f1a5
Md.Arafat Islam Nazmul <rahmannazmul10@gmail.com>
2025-05-10 16:00:21 +0600

    Add files via upload

---
+++ practice_dart.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+void main (){
+  /*stdout.write("Enter the age:");
+  int age= int.parse(stdin.readLineSync()!);
+  stdout.write("Enter the income:");
+  int income= int.parse(stdin.readLineSync()!);
+  print("is there any job of you:(yes/no)");
+  String job=stdin.readLineSync()!.toLowerCase();
+  print("is there any running loan:(yes/no)");
+  String loan=stdin.readLineSync()!.toLowerCase();
+  if(age<21){
+    print("you are not able for loan");
+  }else if(job=='no'){
+    print("sorry tomar jonno loan nai");
+  }else{
+    if(income<30000){
+      print("incom kom loan hbe nah");
+    }else if(loan=='yes'){
+      print("age oi loan porisodh kro");
+    }else{
+      print("tmi loan paba");
+    }
+  }*/
+/*print("akhn koyta baje?");
+double hour=double.parse(stdin.readLineSync()!);
+if(hour>=5 && hour<=11){
+  print("good morning");
+}else if(hour>=12&&hour<=16){
+  print('good dupur');
+}else if (hour>=17&&hour<=20){
+  print('goood evening');
+}else if((hour>=21&&hour<=23)||(hour>=0&&hour<=4)){
+  print("good night");
+  }else{
+  print('you give wrong time');
+  }*/
+}
